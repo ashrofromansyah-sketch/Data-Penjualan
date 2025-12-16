5b93503910626163f081566836ce6943dab79f7a
unknown <nabilahrobbaniyah@gmail.com>
2025-12-16 21:31:54 +0700

    menambahkan fungsi toko yang menjual barang tertentu

--- code/mll.h
+++ code/mll.h
@@ -5,25 +5,31 @@
 #include <string>
 using namespace std;
 
-/* ===== Struktur Data ===== */
+//node
 struct Barang {
     string idBarang;
     string namaBarang;
+    int hargaBarang;
     Barang* next;
+    struct Relasi* firstRelasiBarang;
 };
 
 struct Relasi {
+    Toko* toko;
     Barang* barang;
-    Relasi* next;
+    Relasi* nextRelasiToko;
+    Relasi* nextRelasiBarang;
 };
 
 struct Toko {
     string idToko;
     string namaToko;
+    double ratingToko;
     Toko* next;
-    Relasi* firstRelasi;
+    struct Relasi* firstRelasiToko;
 };
 
+//list
 struct ListBarang {
     Barang* head;
     Barang* tail;
@@ -41,22 +47,29 @@ void createListBarang(ListBarang &LB);
 void insertFirstToko(ListToko &LT, string id, string nama);
 void insertLastToko(ListToko &LT, string id, string nama);
 void insertLastBarang(ListBarang &LB, string id, string nama);
+void insertFirstBarang(ListBarang &LB, string id, string nama);
 
 // Tampil
 void showAllToko(ListToko &LT);
+void showAllBarang(ListBarang &LB);
 void showTokoAndBarang(ListToko &LT);
 
 // Search
 Toko* findToko(ListToko &LT, string key);
 Barang* findBarang(ListBarang &LB, string key);
+//cari realasi child dari toko atau barang
 void searchBarangInToko(Toko* toko, string key);
+
+//toko yang menjual barang tertentu
+void tokoMenjualBarang(ListToko &LT, string idBarang);
 
 // Relasi
 void addRelasi(Toko* toko, Barang* barang);
 
 // Delete
-void deleteAllRelasi(Toko* toko);
+void deleteRelasi(Toko* toko);
 void deleteToko(ListToko &LT, string id);
+void deleteBarang(ListBarang &LB, string id);
 void deleteBarangFromToko(Toko* toko, string idBarang);
 
 // Count
